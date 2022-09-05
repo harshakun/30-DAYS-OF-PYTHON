808e9b9f94e234ce88023e65c567da8bcb35c6d3
harshakun <112824664+harshakun@users.noreply.github.com>
2022-09-05 23:10:29 +0530

    Add files via upload

---
+++ area  of circle using heron.py
@@ -0,0 +1,7 @@
+a = int (input("enter value of 1st side "))
+b = int (input("enter value of 2nd side "))
+c = int (input("enter value of 3rd side "))
+
+s = (a+b+c)/2
+
+print("area using heron formula is ",(s*(s-a)*(s-b)*(s-c))**0.5)         
